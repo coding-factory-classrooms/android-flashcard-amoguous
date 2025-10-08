fab3f469061a79814cdbf415ebce0b91d6365280
Nayash <shayan.cahelo@edu.esiee-it.fr>
2025-10-08 14:20:46 +0200

    Squashed commit of the following:
    commit aebd5bef9ab865be828c1b64c667d9c191f85079
    Author: Nayash <shayan.cahelo@edu.esiee-it.fr>
    Date:   Wed Oct 8 14:17:29 2025 +0200
    
        add | recycler view question list

--- app/src/main/java/com/example/flashcard/QuestionListActivity.java
+++ app/src/main/java/com/example/flashcard/QuestionListActivity.java
@@ -7,8 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuestionListActivity extends AppCompatActivity {
+
+    private List<QuestionList> questionLists;
+    private QuestionListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,21 @@ public class QuestionListActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        questionLists = new ArrayList<>();
+
+        for (int i = 0; i < 2; i++) {
+            questionLists.add(new QuestionList("Qui ête vous ?"));
+            questionLists.add(new QuestionList("Qui sommes nous ?"));
+            questionLists.add(new QuestionList("Pourquoi ?"));
+            questionLists.add(new QuestionList("Et voilà ?"));
+        }
+
+        adapter = new QuestionListAdapter(questionLists);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
