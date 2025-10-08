c4937f9dae23bed0e704aee9ae7ff31b3f2cf454
KizaGoat <badis.oubaiche@edu.esiee-it.fr>
2025-10-08 10:46:16 +0200

    story 1 and 2 finish

--- app/src/main/java/com/example/flashcard/QuizActivity.java
+++ app/src/main/java/com/example/flashcard/QuizActivity.java
@@ -1,6 +1,11 @@
 package com.example.flashcard;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,40 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class QuizActivity extends AppCompatActivity {
 
+    // réponse correcte
+    private final String bonneReponse = "ouali";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_quiz);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        RadioGroup radioGroup = findViewById(R.id.answersRadioGroup);
+        Button validateButton = findViewById(R.id.validateButton);
+
+        validateButton.setOnClickListener(v -> {
+            int selectedId = radioGroup.getCheckedRadioButtonId();
+
+            if (selectedId == -1) {
+                Toast.makeText(this, "selectionne une reponse", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            RadioButton selectedRadioButton = findViewById(selectedId);
+            String selectedText = selectedRadioButton.getText().toString();
+
+            if (selectedText.equalsIgnoreCase(bonneReponse)) {
+                Toast.makeText(this, " bonne reponse !", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, " mauvaise reponse sale noeille c'etait ouali ! : " + bonneReponse, Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
