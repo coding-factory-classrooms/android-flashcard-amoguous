d5750b7a7538a534b260e9ba72653d970ba6a953
Nayash <shayan.cahelo@edu.esiee-it.fr>
2025-10-08 10:47:04 +0200

    Squashed commit of the following:
    commit f7efec90f49a886e969469f4f4de5380f7bb5a84
    Author: Nayash <shayan.cahelo@edu.esiee-it.fr>
    Date:   Wed Oct 8 10:15:57 2025 +0200
    
        Add | dynamically display app version in About screen
    
    commit f7735f5c9da1db2a76c920459272488272062ebd
    Author: Nayash <shayan.cahelo@edu.esiee-it.fr>
    Date:   Tue Oct 7 20:18:38 2025 +0200
    
        Add | about_fab_animation

--- app/src/main/java/com/example/flashcard/AboutActivity.java
+++ app/src/main/java/com/example/flashcard/AboutActivity.java
@@ -1,12 +1,16 @@
 package com.example.flashcard;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -20,5 +24,20 @@ public class AboutActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView versionTextView = findViewById(R.id.versionTextView);
+
+        try {
+            // Get the PackageManager instance — it provides information about all installed apps
+            PackageManager pm = this.getPackageManager();
+            // '0' means no special flags are used
+            PackageInfo pInfo = pm.getPackageInfo(this.getPackageName(), 0);
+            String version = pInfo.versionName;
+
+            versionTextView.setText("Version : " + version);
+        } catch (PackageManager.NameNotFoundException e) {
+            versionTextView.setText("Version : inconnue");
+            e.printStackTrace();
+        }
     }
 }
