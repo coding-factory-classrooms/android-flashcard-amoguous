eb90a219a6747e68bda3702f23c75ba788a1f700
Nayash <shayan.cahelo@edu.esiee-it.fr>
2025-10-10 14:56:14 +0200

    feat | launch QuizActivity with question list

--- app/src/main/java/com/example/flashcard/QuestionListAdapter.java
+++ app/src/main/java/com/example/flashcard/QuestionListAdapter.java
@@ -1,5 +1,8 @@
 package com.example.flashcard;
 
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,10 +11,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // 1. The adapter now works with a list of Question objects
-public class QuestionListAdapter extends RecyclerView.Adapter<QuestionListAdapter.ViewHolder> {
+public class QuestionListAdapter extends RecyclerView.Adapter<QuestionListAdapter.ViewHolder> implements View.OnClickListener{
 
     private List<Question> questionList;
 
@@ -34,11 +38,33 @@ public class QuestionListAdapter extends RecyclerView.Adapter<QuestionListAdapte
         Question currentQuestion = questionList.get(position);
         // 3. We use its getQuestion() method to set the text
         holder.question.setText(currentQuestion.getQuestion());
+
+        holder.itemView.setTag(currentQuestion);
+        holder.itemView.setOnClickListener(this);
+
     }
 
     @Override
     public int getItemCount() {
         return questionList.size();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.i("QuestionListAdapter", "onClick: CLASS");
+
+        if (v.getId() == R.id.rootItem) {
+            Context context = v.getContext();
+            Question q = (Question) v.getTag();
+
+            Intent intent = new Intent(context, QuizActivity.class);
+
+            ArrayList<Question> singleQuestionList = new ArrayList<>();
+            singleQuestionList.add(q);
+
+            intent.putParcelableArrayListExtra("QUESTIONS_LIST", singleQuestionList);
+            context.startActivity(intent);
+        }
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
